069b373583fa547781eda55122ed4054a4f4c228
Jonathan Li <johnnyli@stanford.edu>
2019-12-07 13:43:54 -0800

    fix bugs

--- utils.py
+++ utils.py
@@ -95,8 +95,15 @@ def get_q_prime_k(subgraph,graph,layer):
     n = len(graph)
     e_kk = subgraph.number_of_edges()
     d_k = sum([d for n,d in graph.degree(subgraph.nodes)])
-    p_k = 2*e_kk/(n_k*(n_k - 1))
-    q_k = (d_k - 2*e_kk)/(n_k*(n - n_k))
+    if n_k*(n_k - 1) == 0:
+        p_k = e_kk
+    else:
+        p_k = 2*e_kk/(n_k*(n_k - 1))
+    if n_k*(n - n_k) == 0:
+        q_k = (d_k - 2*e_kk)/n
+        print("n-n_k or n_k is 0")
+    else:
+        q_k = (d_k - 2*e_kk)/(n_k*(n - n_k))
     q_prime_k = q_k/p_k
     return q_prime_k
 
@@ -118,7 +125,7 @@ def reduceEdge(graph,layer):
     relaxed_graph = graph.copy()
     for subgraph in layer:
         q_prime_k = get_q_prime_k(subgraph,graph,layer)
-        edges_to_remove = [edge for edge in subgraph.edges if random.random() < q_prime_k]
+        edges_to_remove = [edge for edge in subgraph.edges if random.random() < (1-q_prime_k)]
         relaxed_graph.remove_edges_from(edges_to_remove)
     return relaxed_graph
 
@@ -142,9 +149,10 @@ def reduceWeight(graph,layer):
     relaxed_graph = graph.copy()
     for subgraph in layer:
         q_prime_k = get_q_prime_k(subgraph,graph,layer)
+        #print("q_prime_k",q_prime_k)
         for u, v, weight in relaxed_graph.edges.data('weight'):
             #print(q_prime_k,relaxed_graph.edges[u, v]['weight'])
-            relaxed_graph.edges[u, v]['weight'] *= q_prime_k
+            relaxed_graph.edges[u, v]['weight'] *= (1+q_prime_k)
     return relaxed_graph
 
 
@@ -180,11 +188,11 @@ def refinement(layers,G):
         for j in range(len(layers)):
             if j == i:
                 continue
-            G_reduced = removeEdge(G_reduced,layers[j])
+            G_reduced = reduceWeight(G_reduced,layers[j])
         partition = louvain(G_reduced)
         m = modularity(partition,G_reduced)
-        output_layers.append(partition_to_layer(partition,G))
-    return output_layers
+        output_layers.append(partition_to_layer(partition,G_reduced))
+    return output_layers, G_reduced
 
 def hicode(G, num_layers = 1):
     #Identification
@@ -195,7 +203,7 @@ def hicode(G, num_layers = 1):
     layers.append(layer)
     for i in range(num_layers-1):
         print("iteration: ",i)
-        G_curr = reduceEdge(G_curr,layer)
+        G_curr = reduceWeight(G_curr,layer)
         partition = louvain(G_curr)
         layer = partition_to_layer(partition,G_curr)
         layers.append(layer)
@@ -206,10 +214,10 @@ def hicode(G, num_layers = 1):
     R_t = 0
     for i in range(10):
         print(i)
-        layers = refinement(layers,G_curr)
+        layers, G_curr = refinement(layers,G_curr)
         Q = average_modularity(layers,G_curr)
         R_t += Q
 
     R_t /= 10*Q_0
 
-    return R_t
+    return R_t, G_curr
